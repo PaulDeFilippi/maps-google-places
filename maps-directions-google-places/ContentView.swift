2a8e897731685e594e5a562fe126eb688970a4d4
Paul Defilippi <allinfo.pd@gmail.com>
2019-12-14 17:19:51 -0500

    Initial Commit

---
+++ maps-directions-google-places/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  maps-directions-google-places
+//
+//  Created by Paul Defilippi on 12/14/19.
+//  Copyright © 2019 Paul Defilippi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
